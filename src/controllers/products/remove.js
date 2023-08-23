7a9e0ebb62880959c675437f5b9885a661242adf
Ale-253 <ale.santamarina@hotmail.com>
2023-08-23 00:29:05 -0300

    se empieza CRUD: Create y se definen campos de products.json

---
+++ src/controllers/products/remove.js
@@ -0,0 +1,12 @@
+/* const { readJSON, writeJSON } = require("../../data")
+
+module.exports = (req, res) => {
+    const products = readJSON('products.json');
+    const id = req.params.id;
+
+    const productsModify = products.filter(product => product.id !== id);
+
+    writeJSON(productsModify, 'products.json')
+
+    return res.redirect('/admin')
+} */
