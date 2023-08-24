8c7e83422b4f42e2488ed1ea13c1959b2fbdbab8
unknown <solvargas094@gmail.com>
2023-08-24 13:20:01 -0300

    add detail,search,home

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
-const { index } = require('../controllers/indexController')
+const { index, search } = require('../controllers/indexController')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', index )
-
+router.get('/search', search); 
 module.exports = router;
