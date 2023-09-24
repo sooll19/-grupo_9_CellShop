1ad77906a615b9cf217d3f1f13dd2b5fdfdfc244
roxfernandez <roxanafernandez4270@gmail.com>
2023-09-24 18:51:33 -0300

    subiendo cambios

--- src/controllers/users/updateProfile.js
+++ src/controllers/users/updateProfile.js
@@ -2,13 +2,13 @@ const { readJSON, writeJSON } = require("../../data")
 
 module.exports = (req,res) => {
     const users = readJSON('user.json');
-    const {name, surname, birthday, about} = req.body
+    const {nombre, apellido, birthday, about} = req.body
     const usersUpdated = users.map(user => {
         if(user.id === req.session.userLogin.id){
             return {
                 ...user,
-                name: name.trim(),
-                surname: surname.trim(),
+                nombre: nombre.trim(),
+                apellido: apellido.trim(),
                 birthday,
                 about : about.trim(),
             }
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,12 +1,10 @@
 module.exports = {
 
   register: require('./users/register'),
-
   processRegister: require('./users/processRegister'),
   login: require('./users/login'),
-
   processLogin: require('./users/processLogin'),
   profile: require('./users/profile'),
-  logout : require('./users/logout'),
   updateProfile : require ('./users/updateProfile'),
+  logout : require('./users/logout'),
 }
