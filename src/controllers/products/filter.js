534a7bff51df93a15604cd541a7830d34bda2977
Ale-253 <ale.santamarina@hotmail.com>
2023-10-22 21:52:09 -0300

    fix: se arregla celulares accesorios y filter.js

--- src/controllers/products/filter.js
+++ src/controllers/products/filter.js
@@ -1,23 +1,27 @@
-const db = require('../../database/models')
+const db = require('../../database/models');
 
 module.exports = (req, res) => {
-  const products = db.Product.findAll({
-    include: ['brand', 'section', 'images'],
-    where: {
-      brandId: req.query.brand
-    }
-  });
-  const brands = db.Brand.findAll({
-    order: ['name']
-  })
 
-  Promise.all([products, brands])
+  const selectedBrand = req.query.brand;
+
+  const brandFilter = selectedBrand === '-1' ? {} : { brandId: selectedBrand };
+
+  const productsPromise = db.Product.findAll({
+    include: ['brand', 'section', 'images'],
+    where: brandFilter
+  });
+
+  const brandsPromise = db.Brand.findAll({
+    order: ['name']
+  });
+
+  Promise.all([productsPromise, brandsPromise])
     .then(([products, brands]) => {
       return res.render('admin', {
         products,
         brands,
-        brand: req.query.brand
-      })
+        brand: selectedBrand
+      });
     })
-    .catch(error => console.log(error))
-}
+    .catch(error => console.log(error));
+};
