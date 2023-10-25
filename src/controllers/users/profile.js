4d1ffed0e92e7ac29d7d6faab75643dbdca5810e
unknown <solvargas094@gmail.com>
2023-10-24 23:11:29 -0300

    modifique registro y perfil de usuario+

--- src/controllers/users/profile.js
+++ src/controllers/users/profile.js
@@ -1,12 +1,13 @@
 const db = require('../../database/models')
 
 module.exports = (req, res) => {
-    db.User.findByPk(req.session.userLogin.id)
+    db.User.findByPk(req.session.userLogin.id,{include:['addresses']})
         .then(user => {
             const birthday = new Date(user.birthday).toISOString();
-
+            const locationPrimary = user.addresses.find(({isPrimary}) => isPrimary)
             return res.render('profile', {
                 ...user.dataValues,
+                location: locationPrimary,
                 birthday: birthday.split('T')[0]
             })
         })
