271ddedaec25ae4741d197849baaf81ed80ee213
Ale-253 <ale.santamarina@hotmail.com>
2023-11-18 16:32:52 -0300

    se valida formulario registro y modificaciones visuales en footer

---
+++ src/routes/apis.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { checkEmail } = require('../controllers/apiController');
+const router = express.Router();
+
+/* /apis */
+router.get('/check-email', checkEmail);
+
+module.exports = router;
