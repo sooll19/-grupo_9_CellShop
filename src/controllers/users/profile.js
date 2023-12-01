9079dae58050801e286b3c2f521c3a2e37b2fa64
unknown <solvargas094@gmail.com>
2023-12-01 17:38:11 -0300

    provincias

--- src/controllers/users/profile.js
+++ src/controllers/users/profile.js
@@ -1,28 +1,31 @@
-const db = require('../../database/models')
+const db = require("../../database/models");
 
 module.exports = (req, res) => {
-    db.User.findByPk(req.session.userLogin.id,{include:['addresses']})
-        .then(user => {
-            const birthday = user.birthday ? new Date(user.birthday).toISOString() : null;
-            const locationPrimary = user.addresses.find(({isPrimary}) => isPrimary)
-            const province = user.province
-            const  address = user.address
-             const city = user.city
+  db.User.findByPk(req.session.userLogin.id, { include: ["addresses"] })
+    .then((user) => {
+      const birthday = user.birthday
+        ? new Date(user.birthday).toISOString()
+        : null;
+      const locationPrimary = user.addresses.find(({ isPrimary }) => isPrimary);
 
-            
-            
-            
-            return res.render('profile', {
-                ...user.dataValues,
+      const province = user.province; 
+      const address = user.address;
+      const city = user.city;
 
-                location: locationPrimary,
-              birthday: birthday ? birthday.split('T')[0] : null,
-              province: province,
-              address:address,
-              city:city
-              
-    
-            })
-        })
-        .catch(error => console.log(error))
-}
+      res.render("profile", {
+        ...user.dataValues,
+        location: locationPrimary,
+        birthday: birthday ? birthday.split("T")[0] : null,
+        province: province,
+        address: address,
+        city: city,
+      });
+
+      
+      user.province = province;
+      user.address = address;
+      user.city = city;
+      return user.save();
+    })
+    .catch((error) => console.log(error));
+};
